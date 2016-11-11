8fbdd73d21b6a292babb4dcd2479b80195a034d5
Andrew Heins <andrew.heins@gmail.com>
2016-11-10 23:03:18 -0500

    Adds new hooks for removing feeds and some other WordPre cruft

---
+++ hooks/clean-head.php
@@ -0,0 +1,16 @@
+<?php 
+  
+  register_futility_hook(
+    'clean_head',
+    'Removes garbage from &lt;head&gt;',
+    'wp_loaded',
+    function(){
+      remove_action('wp_head', 'rsd_link');
+	    remove_action('wp_head', 'wlwmanifest_link');
+	    remove_action('wp_head', 'wp_generator');
+	    remove_action('wp_head', 'start_post_rel_link');
+	    remove_action('wp_head', 'index_rel_link');
+	    remove_action('wp_head', 'adjacent_posts_rel_link');
+    },
+    "Removes a bunch of stuff you don't need from &lt;head&gt;"
+  );
---
+++ hooks/remove-feeds.php
@@ -0,0 +1,12 @@
+<?php 
+  
+  register_futility_hook(
+    'remove_feeds',
+    'Removes Feeds from &lt;head&gt;',
+    'wp_loaded',
+    function(){
+      remove_action( 'wp_head', 'feed_links', 2 );
+      remove_action( 'wp_head', 'feed_links_extra', 3 );
+    },
+    "Removes basic and comment feeds from WordPress Header"
+  );
