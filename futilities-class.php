0d5975bee475280b66396f523bce48b6143a1315
Andrew Heins <andrew.heins@gmail.com>
2016-11-13 20:46:13 -0500

    Rethinking implementation of Futilities Utility Classes

--- futilities-class.php
+++ futilities-class.php
@@ -47,7 +47,7 @@ class Futilities {
       }
       
     }
-    
+
     // Initialize the settings page
     require_once( 'settings.php' );
     
@@ -59,77 +59,41 @@ class Futilities {
   
   // Our registration functions. These is what you use to register a futility.
   public function register_futility_hook( 
-      $id, // unique id
-      $hook_title, // 10 word description of what the hook does.
-      $hook_bound_to, // what action/filter are you binding to
-      $callback, // The function that actually does the thing
-      $description = '',
-      $type = 'action'
-    ) {
-      
-      $this->futilities['hooks'][$type][] = (object) array(
-        'ID' => $id,
-        'title' => $hook_title,
-        'type' => $type,
-        'binding' => $hook_bound_to,
-        'callback' => $callback,
-        'field' => function( $args ) {
-          
-          $options = get_option( 'futilities_settings' );
-          
-          if( empty( $options ) ) {
-            $state = false;
-          } else {
-            $state = $options["hook_{$args['type']}_{$args['id']}"];
-          }
-          
-          $field = "<label for='futilities_settings[hook_{$args['type']}_{$args['id']}]'><input type='checkbox'";
-          if( $state ) {
-            $field .= " checked ";
-          }
-          $field .= "name='futilities_settings[hook_{$args['type']}_{$args['id']}]'> {$args['description']}</label>";
-          
-          echo $field;
-        },
-        'description' => $description,
-      ); 
-    }
+    $id, // unique id
+    $hook_title, // 10 word description of what the hook does.
+    $hook_bound_to, // what action/filter are you binding to
+    $callback, // The function that actually does the thing
+    $description = '',
+    $type = 'action'
+  ) {
     
-    public function register_futility_class( 
-      $id, // unique id
-      $class_title, // 10 word description of what the hook does.
-      $hook_bound_to, // what action/filter are you binding to
-      $callback, // The function that actually does the thing
-      $description = '',
-      $type = 'action'
-    ) {
-      
-      $this->futilities['hooks'][$type][] = (object) array(
-        'ID' => $id,
-        'title' => $hook_title,
-        'binding' => $hook_bound_to,
-        'callback' => $callback,
-        'field' => function( $args ) {
-          
-          $options = get_option( 'futilities_settings' );
-          
-          if( empty( $options ) ) {
-            $state = false;
-          } else {
-            $state = $options["hook_{$args['type']}_{$args['id']}"];
-          }
-          
-          $field = "<label for='futilities_settings[hook_{$args['type']}_{$args['id']}]'><input type='checkbox'";
-          if( $state ) {
-            $field .= " checked ";
-          }
-          $field .= "name='futilities_settings[hook_{$args['type']}_{$args['id']}]'> {$args['description']}</label>";
-          
-          echo $field;
-        },
-        'description' => $description,
-      ); 
-    }
+    $this->futilities['hooks'][$type][] = (object) array(
+      'ID' => $id,
+      'title' => $hook_title,
+      'type' => $type,
+      'binding' => $hook_bound_to,
+      'callback' => $callback,
+      'field' => function( $args ) {
+        
+        $options = get_option( 'futilities_settings' );
+        
+        if( empty( $options ) ) {
+          $state = false;
+        } else {
+          $state = $options["hook_{$args['type']}_{$args['id']}"];
+        }
+        
+        $field = "<label for='futilities_settings[hook_{$args['type']}_{$args['id']}]'><input type='checkbox'";
+        if( $state ) {
+          $field .= " checked ";
+        }
+        $field .= "name='futilities_settings[hook_{$args['type']}_{$args['id']}]'> {$args['description']}</label>";
+        
+        echo $field;
+      },
+      'description' => $description,
+    ); 
+  }
 
   public static function get_instance() {
 
